9e939e779db45478e1ca2734743171361c3b7b19
nacho <nacho.conso@gmail.com>
2021-03-31 00:14:29 -0300

    add another option

--- tp2/ej9.py
+++ tp2/ej9.py
@@ -1,4 +1,15 @@
+#Opcion 1
 frase = '3s un het3rog74ma2021//))'
 letras = [letra for letra in frase if not (letra.isalpha() and frase.count(letra) > 1)]
 isHeterograma = len(letras) == len(frase)
+print(isHeterograma)
+
+#Opcion 2
+frase = '3s-un-he3rog74ma2021//))'
+isHeterograma = True
+for letra in frase:
+    if letra.isalpha() and frase.count(letra) > 1:
+        isHeterograma = False
+        break
+
 print(isHeterograma)
